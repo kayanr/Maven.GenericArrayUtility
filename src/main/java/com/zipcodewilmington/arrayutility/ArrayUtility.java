45de97528c46e603a704797fbdbb9c1025ac2e0e
Kayan Reid <kreid@Zipcoders-MacBook-Pro-4.local>
2024-12-02 08:26:21 -0500

    Made ArrayUtility class generic. Added constructor and inputArray and arrayToMerge fields to ArrayUtility class.

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,11 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+   T[] inputArray;
+   T[] arrayToMerge;
+
+    public ArrayUtility(T[] inputArray) {
+       this.inputArray = inputArray;
+    }
 }
